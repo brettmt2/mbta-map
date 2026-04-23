d2144aa7879b23464b7f020b2c829335df8e088f
brettmt2 <btpeach.2@gmail.com>
2026-04-23 17:58:05 -0400

    line up loop logic with proper data format in valid filtering

--- app/stop_times.py
+++ app/stop_times.py
@@ -26,22 +26,20 @@ async def get_station_stop_times(client: httpx.AsyncClient, parent_station: str,
     data = data['data']
     return data
 
-def filter_valid_times(line: str, station_data: list):
+def filter_valid_times(station_data: list) -> list[list]:
     now = datetime.now()
     valid = []
-    predictions = None
     
-    for trip in predictions:
-        trip_type = trip.get('relationships').get('route').get('data').get('id')
+    for trip in station_data:
         arrival = trip.get('attributes').get('arrival_time')
 
-        if trip_type == line and arrival:
+        if arrival:
             time = datetime.fromisoformat(arrival).replace(tzinfo=None)
-            if time > now: # valid time and route
+            if time > now: # valid time
                 trip_id = trip.get('relationships').get('trip').get('data').get('id')
                 valid.append([trip_id, time])
 
-    return valid[:5]
+    return valid
 
 async def get_line_times(color: str) -> dict:
     line_data = {}
@@ -53,6 +51,12 @@ async def get_line_times(color: str) -> dict:
         )
     
     for s, s_results in zip(filtered, results):
-        line_data[s] = s_results
+        line_data[s] = filter_valid_times(s_results)
 
-asyncio.run(get_line_times('Red'))
+    return line_data
+
+
+# data = asyncio.run(get_line_times('Red'))
+# print(data['place-harsq'][:2])
+
+
