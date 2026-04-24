577669ffbeca71883a47931bae5449bab084ecd6
brettmt2 <btpeach.2@gmail.com>
2026-04-24 12:50:33 -0400

    pair child route value with data

--- app/stop_times.py
+++ app/stop_times.py
@@ -5,6 +5,7 @@ import asyncio
 
 from datetime import datetime
 from static.stc_stations import stc_stations
+from static.stc_route_patterns import route_patterns
 
 load_dotenv()
 
@@ -13,19 +14,31 @@ headers = {
     'x-api-key': os.getenv('API_KEY')
 }
 
-async def get_station_stop_times(client: httpx.AsyncClient, parent_station: str, color: str) -> list[dict]:
+async def get_station_stop_times(client: httpx.AsyncClient, parent_station: str, color: str) -> dict[str, list[dict]]:
+    child_times = {}
+
     url = 'https://api-v3.mbta.com/predictions'
-    params = {
-        'filter[stop]': parent_station,
-        'filter[route]': color,
-        'sort': 'arrival_time'
-    }
 
-    result = await client.get(url, params=params, headers=headers)
-    data = result.json()
-    data = data['data']
+    data = await asyncio.gather(
+        *[client.get(url, params={
+            'filter[stop]': parent_station,
+            'filter[route]': color,
+            'filter[route_pattern]': rp,
+            'sort': 'arrival_time'
+        }, headers=headers) 
+        for rp in route_patterns[color]]
+    )
 
-    return data
+    for (rp, dest), res in zip(route_patterns[color].items(), data):
+        rp_data: list = res.json()['data']
+        if len(rp_data) > 0:
+            # check if destination data exists for a child route yet
+            if dest not in child_times:
+                child_times[dest] = rp_data
+            else:
+                child_times[dest] = child_times[dest] + rp_data
+
+    return child_times
 
 def filter_valid_times(station_data: list) -> list[list]:
     now = datetime.now()
@@ -42,6 +55,21 @@ def filter_valid_times(station_data: list) -> list[list]:
 
     return valid
 
+async def get_line_times(color: str) -> dict[str, dict[str, list[dict]]]:
+    line_data = {}
+    filtered = [station for station in stc_stations if color in stc_stations[station].get('route')]
+
+    async with httpx.AsyncClient() as client:
+        results = await asyncio.gather(
+            *[get_station_stop_times(client, station, color) for station in filtered]
+        )
+
+    for s, s_results in zip(filtered, results):
+        line_data[s] = s_results
+    
+    return line_data
+
+# old approach, found a better solution. this was too many api calls
 async def get_child_headsigns(client: httpx.AsyncClient, trip_id: str):
     url = f'https://api-v3.mbta.com/trips/{trip_id}'
     result = await client.get(url, headers=headers) 
@@ -51,34 +79,6 @@ async def get_child_headsigns(client: httpx.AsyncClient, trip_id: str):
 
     return data.get('attributes').get('headsign')
 
-async def get_line_times(color: str) -> dict:
-    line_data = {}
-    filtered = [station for station in stc_stations if color in stc_stations[station].get('route')]
-
-    async with httpx.AsyncClient() as client:
-        results = await asyncio.gather(
-            *[get_station_stop_times(client, station, color) for station in filtered]
-        )
-
-        for s, s_results in zip(filtered, results):
-            line_data[s] = filter_valid_times(s_results)
-
-        # time[0] = trip id, time[1] = time, assigning time[2] to be headsign
-        for s in line_data:
-            headsigns = await asyncio.gather(
-                *[get_child_headsigns(client, time[0]) for time in line_data[s]]
-            )
-
-            for time, headsign in zip(line_data[s], headsigns):
-                time.append(headsign)
-
-    return line_data
-
 
 data = asyncio.run(get_line_times('Red'))
-
-for station in data:
-    if station == 'place-harsq':
-        print(data[station])
-
-
+x = data['place-harsq']
